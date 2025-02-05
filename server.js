b09dc912fb6e6d44dfbde8c689083532712513f0
Preetham Praveen <preethampraveencode26@gmail.com>
2025-02-05 11:21:56 +0530

    api

---
+++ server.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const express = require('express')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+const menu = require('./schema')
+
+mongoose.connect(process.env.DB_URL)
+.then(()=>{
+    console.log("Connected")
+})
+.catch(()=>{
+    console.log("Error")
+})
+
+
+app.post("/menu", async(req,res)=>{
+    try{
+        const {name,description,price} = req.body
+        const newItem = new menu({ name, description, price });
+        await newItem.save();
+        res.status(201).json({ message: "Menu item added", item: newItem });
+    }
+    catch(error){
+        res.status(500).json({ error: "Server error" });
+    }
+})
+
+app.get("/menu", async(req,res)=>{
+    try{
+        const all = await menu.find();
+        res.json(all)
+    }
+    catch(error){
+        res.status(500).json({ error: "Server error" });
+    }
+})
+
+app.listen(3000,()=>{
+    console.log("Running")
+})
